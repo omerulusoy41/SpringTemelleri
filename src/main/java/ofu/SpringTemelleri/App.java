d2085d1123f8417b440a81691a5d113f684c07dc
Ömer Faruk <ulusoyomerfaruk29@gmail.com>
2023-03-13 22:26:43 +0300

    lazy-init islendi

--- src/main/java/ofu/SpringTemelleri/App.java
+++ src/main/java/ofu/SpringTemelleri/App.java
@@ -12,6 +12,8 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("ofu/SpringTemelleri/config.xml");
+        System.out.println("lazy-init=true oldugundan beanA ve beanB cagirildiginda olustu");
+        System.out.println("Normalde context newlenirken olusacakti");
     	BeanA beanA = (BeanA) context.getBean("BeanA");
     	BeanB beanB = context.getBean("BeanB",BeanB.class);
     	System.out.println(beanA);
