3e96922ca28192fc8497094e2489dd184dbe0149
Ömer Faruk <ulusoyomerfaruk29@gmail.com>
2023-03-13 22:58:49 +0300

    scope=singleton and scope=prototype diffrence

--- src/main/java/ofu/SpringTemelleri/App.java
+++ src/main/java/ofu/SpringTemelleri/App.java
@@ -12,11 +12,17 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("ofu/SpringTemelleri/config.xml");
-        System.out.println("lazy-init=true oldugundan beanA ve beanB cagirildiginda olustu");
-        System.out.println("Normalde context newlenirken olusacakti");
-    	BeanA beanA = (BeanA) context.getBean("BeanA");
-    	BeanB beanB = context.getBean("BeanB",BeanB.class);
-    	System.out.println(beanA);
-    	System.out.println(beanB);
+        BeanC beanc1 = context.getBean("BeanC",BeanC.class);
+        BeanC beanc2 = context.getBean("BeanC",BeanC.class);
+        if (beanc1 == beanc2)
+        	System.out.println("Ben singletonum");
+        else 
+        	System.out.println("Ben prototypeim");
+//        System.out.println("lazy-init=true oldugundan beanA ve beanB cagirildiginda olustu");
+//        System.out.println("Normalde context newlenirken olusacakti");
+//    	BeanA beanA = (BeanA) context.getBean("BeanA");
+//    	BeanB beanB = context.getBean("BeanB",BeanB.class);
+//    	System.out.println(beanA);
+//    	System.out.println(beanB);
     }
 }
---
+++ src/main/java/ofu/SpringTemelleri/BeanC.java
@@ -0,0 +1,9 @@
+package ofu.SpringTemelleri;
+
+
+public class BeanC {
+	
+	public BeanC() {
+		System.out.println("ben BeanC");
+	}
+}
