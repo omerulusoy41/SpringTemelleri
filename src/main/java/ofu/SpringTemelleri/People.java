d8487970c333294d9d60404abf04d8c02896b365
Ömer Faruk <ulusoyomerfaruk29@gmail.com>
2023-03-15 21:04:21 +0300

    Injectionlari setter mehodlarda hallediceksem static tanımlamama gerek kalmadı

--- src/main/java/ofu/SpringTemelleri/People.java
+++ src/main/java/ofu/SpringTemelleri/People.java
@@ -1,5 +1,6 @@
 package ofu.SpringTemelleri;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -9,32 +10,39 @@ public class People {
 	private String name;
 	private String lastname;
 	private int age;
-	
-	public People(String name, String lastname, int age) {
-		super();
-		this.name = name;
-		this.lastname = lastname;
-		this.age = age;
-		System.out.println("olustum");
-	}
 
 	@Override
 	public String toString() {
 		return "People [name=" + name + ", lastname=" + lastname + ", age=" + age + "]";
 	}
 	
+	@Autowired
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Autowired
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	@Autowired
+	public void setAge(int age) {
+		this.age = age;
+	}
+
 	@Bean("name")
-	public static String setName() {
+	public  String setName() {
 		return "Omer";
 	}
 	
 	@Bean ("lastname")
-	public static String setLastname() {
+	public  String setLastname() {
 		return "ulusoy";
 	}
 	
 	@Bean ("age")
-	public static int setAge() {
+	public  int setAge() {
 		return 20;
 	}
 	
